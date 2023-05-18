6a349105aacc2a3284ce24d409d116ac5df23c73
marcelo.cavalcanti <marcelodemoura14@gmail.com>
2023-05-18 08:40:36 -0300

    "primeiro commmit"

---
+++ novocurso/src/main/java/com/testeaula/novocurso/NovoCursoApplication.java
@@ -0,0 +1,13 @@
+package com.testeaula.novocurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoCursoApplication.class, args);
+	}
+
+}
